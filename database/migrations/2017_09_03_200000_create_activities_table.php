8409fae5194d8b83ce91194b975201d7fdff2a94
Kevin <kjdion84@gmail.com>
2017-10-09 23:41:25 -0400

    added activity indexes

--- database/migrations/2017_09_03_200000_create_activities_table.php
+++ database/migrations/2017_09_03_200000_create_activities_table.php
@@ -12,10 +12,10 @@ class CreateActivitiesTable extends Migration
         Schema::create('activities', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->nullable();
-            $table->integer('model_id')->nullable();
-            $table->string('model_class')->nullable();
+            $table->integer('model_id')->nullable()->index();
+            $table->string('model_class')->nullable()->index();
             $table->text('data')->nullable();
-            $table->string('log');
+            $table->string('log')->index();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
